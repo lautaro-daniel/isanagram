d853fc41cdba2df49562418d86d6919e4e15debf
lautaro-daniel <lauty.9984@gmail.com>
2024-01-04 18:52:33 -0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import functions.Function;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Function function = new Function();
+
+        System.out.println(function.isAnagram("amor", "roma"));
     }
 }
--- src/functions/Function.java
+++ src/functions/Function.java
@@ -1,4 +1,15 @@
 package functions;
 
+import java.util.Arrays;
+
 public class Function {
+    public boolean isAnagram(String word1, String word2) {
+        char[] arrayOne = word1.toCharArray(); //separate the word in char array
+        char[] arrayTwo = word2.toCharArray();
+
+        Arrays.sort(arrayOne);//order the letters
+        Arrays.sort(arrayTwo);
+
+        return Arrays.equals(arrayOne, arrayTwo);//compare the arrays to know if they're equals
+    }
 }
